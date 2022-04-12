0ecd7eb7babd68b37a316bb80369ffd369600a4b
Authachai Murnpimai <premelar7@gmail.com>
2022-04-12 23:48:35 +0700

    Update SSR

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // pages/index.js
 
-export default function Home(props) {
+export default function Home() {
     return (
         <div>
             <div>
@@ -8,41 +8,6 @@ export default function Home(props) {
                     LWRY :: Public Website V1.0.2
                 </h1>
             </div>
-            <div>
-                {
-                    props.isSSR ? (
-                        <h2>
-                            SSR Working
-                        </h2>
-                    ) : (
-                        <h2>
-                            SSR Not Works
-                        </h2>
-                    )
-                }
-            </div>
-
-            <div>
-                {
-                    props.isStatic ? (
-                        <h2>
-                            Static generated
-                        </h2>
-                    ) : (
-                        <h2>
-                            is Not Static
-                        </h2>
-                    )
-                }
-            </div>
         </div>
     )
-}
-
-// export async function getStaticProps() {
-//     return { props: { isStatic: true } }
-// }
-
-export async function getServerSideProps() {
-    return { props: { isSSR: true } }
 }
