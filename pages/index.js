c846c9fc40f5ba564bce8211d90d784f64b0b035
Authachai Murnpimai <premelar7@gmail.com>
2022-04-12 22:48:36 +0700

    Test Server Site Rendering

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,48 @@
-export default () => <h1>LWRY :: public website Next.js V1.0.2</h1>
+// pages/index.js
+
+export default function Home(props) {
+    return (
+        <div>
+            <div>
+                <h1>
+                    LWRY :: Public Website V1.0.2
+                </h1>
+            </div>
+            <div>
+                {
+                    props.isSSR ? (
+                        <h2>
+                            SSR Working
+                        </h2>
+                    ) : (
+                        <h2>
+                            SSR Not Works
+                        </h2>
+                    )
+                }
+            </div>
+
+            <div>
+                {
+                    props.isStatic ? (
+                        <h2>
+                            Static generated
+                        </h2>
+                    ) : (
+                        <h2>
+                            is Not Static
+                        </h2>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
+
+export async function getStaticProps() {
+    return { props: { isStatic: true } }
+}
+
+export async function getServerSideProps() {
+    return { props: { isSSR: true } }
+}
